f551fcec6a2fc5b7d90b0c39629be21c2d2f605b
Raul Sanchez R. <raul.sr92@gmail.com>
2024-10-30 12:21:41 -0500

    Clase 22 - Enemigos aleatorios

--- js/mokepon.js
+++ js/mokepon.js
@@ -74,15 +74,65 @@ function seleccionarMascotaJugador(){
             default:
                 alert(`No has seleccionado ninguna mascota`)
                 break;
-        }
+        } 
 
-
-
-
-
-        
-        
+        seleccionarMascotaEnemigo();
 }
+
+
+
+// Clase 21: Crear funcion para obtener un numero aleatorio entre un rango
+
+function aleatorio(max, min) {
+
+    return Math.floor(Math.random() * (max - min+1) + min)
+}
+
+function seleccionarMascotaEnemigo(){
+    
+    // Capturar mascotas en variables
+
+    let moke1 = document.getElementById("hipodoge");
+    let moke2 = document.getElementById("capipepo");
+    let moke3 = document.getElementById("ratigueya");
+    let moke4 = document.getElementById("langostelvis");
+    let moke5 = document.getElementById("tucapalma");
+    let moke6 = document.getElementById("pydos");
+
+    // Capturar elemento <span></span>
+
+    let showMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+    // Mostrar la mascota seleccionada
+
+    let aleatorioEnemigo = aleatorio(6,1);
+    console.log(aleatorioEnemigo);
+
+    switch (aleatorioEnemigo) {
+        case 1:
+            showMascotaEnemigo.innerHTML = moke1.value
+            break;
+
+        case 2:
+            showMascotaEnemigo.innerHTML = moke2.value
+            break;    
+        case 3:
+            showMascotaEnemigo.innerHTML = moke3.value
+            break;  
+        case 4:
+            showMascotaEnemigo.innerHTML = moke4.value
+            break;  
+        case 5:
+            showMascotaEnemigo.innerHTML = moke5.value
+            break;                
+        default:
+            showMascotaEnemigo.innerHTML = moke6.value
+            break;
+    } 
+}
+
+
+// Inicio del juego
 
 window.addEventListener("load", iniciarJuego);
 
